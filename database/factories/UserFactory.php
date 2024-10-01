074ef6b3d6368e1ed8b60be70f55b86644f0d5d6
Abbass <abbassmortazavi@gmail.com>
2024-10-01 19:36:41 +0330

    bug fix

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 /**
@@ -22,12 +23,11 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
-
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => 'ddddddd',
+            'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
     }
